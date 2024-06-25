e78ba1c060c8bce9df7993c2b867f766a238224f
ajlaroya <arthur.j.laroya@gmail.com>
2024-06-25 13:03:52 +1000

    landing page complete, scroll navbar, dark mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/components/providers/theme-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,10 +12,10 @@ export const metadata: Metadata = {
     icon: [
       {
         url: "/logo.svg",
-        href: "/logo.svg" 
-      }
-    ]
-  }
+        href: "/logo.svg",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
@@ -23,8 +24,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body className={inter.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+          storageKey="jotion-theme"
+        >
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
